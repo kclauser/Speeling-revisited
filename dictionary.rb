a0fa21a7671c7f608d3d0aa48b89912810f1eab4
Kurtis Clauser <kclauser@gmail.com>
2015-09-16 21:09:30 -0400

    pre erb changes

--- dictionary.rb
+++ dictionary.rb
@@ -1,11 +1,13 @@
 require 'webrick'
+require 'yaml'
+require 'erb'
 
 class CurrentWords < WEBrick::HTTPServlet::AbstractServlet
 
   def do_GET(request, response)
 
-    if File.exist?("words.txt")
-      words = File.readlines("words.txt")
+    if File.exist?("words.yml")
+      words = File.readlines("words.yml")
     else
       words = []
     end
@@ -13,16 +15,16 @@ class CurrentWords < WEBrick::HTTPServlet::AbstractServlet
     response.status = 200
     response.body = %{
       <html>
-        <body>
-        <form method="POST" action="/search"
-        <ul>
-          <li><input name="searchword"/><li>
-        </ul>
-        <button type="submit">Search!</button>
-        </form>
-        <a href="/add">"Add word"</a>
-        <p>#{words.join("<br/>")}</p>
-        </body>
+      <body>
+      <form method="POST" action="/search"
+      <ul>
+      <li><input name="searchword"/><li>
+      </ul>
+      <button type="submit">Search!</button>
+      </form>
+      <a href="/add">"Add word"</a>
+      <p>#{words.join("<br/>")}</p>
+      </body>
       </html>
     }
   end
@@ -30,37 +32,37 @@ end
 
 class AddWord < WEBrick::HTTPServlet::AbstractServlet
 
-def do_GET(request, response)
+  def do_GET(request, response)
 
-  if File.exist?("words.txt")
-    words = File.readlines("words.txt")
-  else
-    words = []
-  end
+    if File.exist?("words.yml")
+      words = File.readlines("words.yml")
+    else
+      words = []
+    end
 
-  response.status = 200
-  response.body = %{
-    <html>
+    response.status = 200
+    response.body = %{
+      <html>
       <body>
 
-    <!--#{words.join("<br/")}-->
+      <!--#{words.join("<br/")}-->
       <form method="POST" action="/save">
-        <ul>
-          <li><input name="word"/></li><- word
-          <li><input name="definition"/></li><- definition
-        </ul>
-        <button type="submit">Submit!</button>
+      <ul>
+      <li><input name="word"/></li><- word
+      <li><input name="definition"/></li><- definition
+      </ul>
+      <button type="submit">Submit!</button>
       </form>
       </body>
-    </html>
-  }
+      </html>
+    }
 
-end
+  end
 end
 
 class SaveWord < WEBrick::HTTPServlet::AbstractServlet
   def do_POST(request, response)
-    File.open("words.txt", "a+") do |file|
+    File.open("words.yml", "a+") do |file|
       file.puts "Word: #{request.query["word"]} Definition: #{request.query["definition"]}"
     end
 
@@ -72,20 +74,20 @@ end
 
 class SearchWord < WEBrick::HTTPServlet::AbstractServlet
   def do_POST(request, response)
-    lines = File.readlines("words.txt")
+    lines = File.readlines("words.yml")
     matching_lines = lines.select { |line| line.include?(request.query["searchword"])}
     html = "<ul>" + (matching_lines.map { |line| "<li>#{line}</li>"}).join + "</ul>"
 
-response.status = 200
-response.body = %{
-<html>
-<body>
-#{html}
-</body>
-</html>
+    response.status = 200
+    response.body = %{
+      <html>
+      <body>
+      #{html}
+      </body>
+      </html>
 
-}
-end
+    }
+  end
 end
 
 server = WEBrick::HTTPServer.new(Port: 3000)
